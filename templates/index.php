79f37c05c80ef6f19ce3dc002a4443b496d3ce96
davidenko.sinergium.ru <stariyznakomiy0@gmail.com>
2023-07-25 17:30:01 +0300

    Модуль 3 Шаблонизация и организация кода
    Шаблонизация и организация кода + работа с датой и временем

---
+++ templates/index.php
@@ -0,0 +1,52 @@
+
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
+            </li>
+            <li class="promo__item promo__item--attachment">
+                <a class="promo__link" href="all-lots.html">Крепления</a>
+            </li>
+            <li class="promo__item promo__item--boots">
+                <a class="promo__link" href="all-lots.html">Ботинки</a>
+            </li>
+            <li class="promo__item promo__item--clothing">
+                <a class="promo__link" href="all-lots.html">Одежда</a>
+            </li>
+            <li class="promo__item promo__item--tools">
+                <a class="promo__link" href="all-lots.html">Инструменты</a>
+            </li>
+            <li class="promo__item promo__item--other">
+                <a class="promo__link" href="all-lots.html">Разное</a>
+            </li>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <?php foreach ($ads as $ad) : ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?= $ad["url"] ?>" width="350" height="260" alt="Сноуборд">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?= $ad["Категория"] ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $ad["Название"] ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?= priceFormat($ad["Цена"]) ?></span>
+                            </div>
+                            <div class="lot__timer timer">
+                              <?= timer("26.07.2023") ?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </section>
